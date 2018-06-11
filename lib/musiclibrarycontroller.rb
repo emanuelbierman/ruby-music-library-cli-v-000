ceb160bc9d298688cbf7fc9b941d74eb24f824af
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-06-11 17:34:51 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -16,13 +16,14 @@ class MusicLibraryController
   end
 
   def call
-    options = ["list songs",
-      "list artists",
-      "list genres",
-      "list artist",
-      "list genre",
-      "play song",
-      "exit"]
+    # options = ["list songs",
+    #   "list artists",
+    #   "list genres",
+    #   "list artist",
+    #   "list genre",
+    #   "play song",
+    #   "exit"]
+    user_input = ""
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
